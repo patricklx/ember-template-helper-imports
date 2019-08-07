d8ce6ce8aece1f4db0979fae910786cd5e568293
Patrick Pircher <patrick.pircher@ibm.com>
2019-08-07 11:19:08 +0200

    call existing resolveHelper

--- app/initializers/setup-template-helper-resolver.js
+++ app/initializers/setup-template-helper-resolver.js
@@ -3,7 +3,14 @@ export default {
   name: 'setup-template-helper-resolver',
   initialize: function initialize(application) {
     let resolver = application.__registry__.resolver._fallback || application.__registry__.resolver;
+    const resolveHelper = resolver.resolveHelper;
     resolver.resolveHelper = function(parsedName) {
+      if (resolveHelper) {
+        const resolved = resolveHelper.call(this, parsedName);
+        if (this._moduleRegistry.has(resolved)) {
+          return this._extractDefaultExport(resolved, parsedName);
+        }
+      }
       let podPrefix = this.namespace.podModulePrefix || this.namespace.modulePrefix;
       let fullNameWithoutType = parsedName.fullNameWithoutType;
 
