87ec048e60c8fa4412392b64dd232a736ad20d13
Patrick Pircher <patrick.pircher@ibm.com>
2019-08-07 15:34:56 +0200

    fix MU resolution and inline helper call

--- app/initializers/setup-template-helper-resolver.js
+++ app/initializers/setup-template-helper-resolver.js
@@ -9,7 +9,7 @@ export default {
         const resolved = resolveHelper.call(this, parsedName);
         if (this._moduleRegistry.has(resolved)) {
           const exp = this._extractDefaultExport(resolved, parsedName);
-          return exp.helper || exp.default;
+          return exp.helper || exp;
         }
       }
       let prefix = this.namespace.podModulePrefix;
@@ -18,19 +18,19 @@ export default {
       let normalizedModuleName = prefix + '/' + fullNameWithoutType;
       if (this._moduleRegistry.has(normalizedModuleName)) {
         const exp = this._extractDefaultExport(normalizedModuleName, parsedName);
-        return exp.helper || exp.default;
+        return exp.helper || exp;
       }
 
       prefix = this.namespace.modulePrefix;
       normalizedModuleName = prefix + '/' + fullNameWithoutType;
       if (this._moduleRegistry.has(normalizedModuleName)) {
         const exp = this._extractDefaultExport(normalizedModuleName, parsedName);
-        return exp.helper || exp.default;
+        return exp.helper || exp;
       }
 
       if (this._moduleRegistry.has(fullNameWithoutType)) {
         const exp = this._extractDefaultExport(fullNameWithoutType, parsedName);
-        return exp.helper || exp.default;
+        return exp.helper || exp;
       }
 
       return null;
