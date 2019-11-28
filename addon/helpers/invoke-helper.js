cbbe463cdef3d515a8aae5655954faf80a426dc7
patricklx <patricklx@users.noreply.github.com>
2019-11-28 19:53:21 +0100

    add error message

--- addon/helpers/invoke-helper.js
+++ addon/helpers/invoke-helper.js
@@ -6,11 +6,14 @@ export const helper = Helper.extend({
   compute([context, helper, ...args], hash) {
     if (!this.h) {
       this.h = getOwner(context).lookup(`helper:${helper}`);
-      if (this.h.create) {
+      if (this.h && this.h.create) {
         const H = this.h;
         this.h = H.create();
         setOwner(this.h, getOwner(context));
         this.h.recompute = this.recompute.bind(this);
+      }
+      if (!this.h || this.h.compute) {
+        throw new Error('could not find helper: ' + helper);
       }
     }
     return this.h.compute(args, hash);
