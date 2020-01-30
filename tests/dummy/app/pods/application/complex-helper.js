68d6e61b8f8619561141f739f671f976d38b4bc5
patricklx <patricklx@users.noreply.github.com>
2020-01-30 13:37:06 +0100

    Update complex-helper.js

--- tests/dummy/app/pods/application/complex-helper.js
+++ tests/dummy/app/pods/application/complex-helper.js
@@ -3,6 +3,11 @@ import { later } from '@ember/runloop';
 
 export const helper = Helper.extend({
   text: 'abc',
+  init(...args) {
+    super.init(...args);
+    const o = Ember.getOwner(this);
+     if (!o) throw new Error('no owner');
+  },
   compute() {
     if (this.text === 'abc') {
       setTimeout(() => {
