887886475601714569d78f53508aad366c499e63
patricklx <patricklx@users.noreply.github.com>
2020-01-30 12:25:05 +0100

    Update invoke-helper.js

--- addon/helpers/invoke-helper.js
+++ addon/helpers/invoke-helper.js
@@ -8,9 +8,15 @@ export const helper = Helper.extend({
       this.h = getOwner(context).lookup(`helper:${helper}`);
       if (this.h && this.h.create) {
         const H = this.h;
-        const props = {};
-        setOwner(props, getOwner(context));
-        this.h = H.create(props);
+        if (H.prototype && H.prototype.init) {
+          const ini = H.prototype.init;
+          H.prototype.init = function(...iniargs) {
+            setOwner(this, getOwner(context));
+            ini.call(this, ...iniargs);
+          }
+        }
+        this.h = H.create();
+        setOwner(this.h, getOwner(context));
         this.h.recompute = this.recompute.bind(this);
       }
       if (!this.h || !this.h.compute) {
