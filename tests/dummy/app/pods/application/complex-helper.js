20484f258c49818b8e5d32c472c2b856c26a940b
patricklx <patricklx@users.noreply.github.com>
2020-01-31 08:11:50 +0100

    set owner on props (#10)

--- tests/dummy/app/pods/application/complex-helper.js
+++ tests/dummy/app/pods/application/complex-helper.js
@@ -1,8 +1,14 @@
 import Helper from '@ember/component/helper';
 import { later } from '@ember/runloop';
+import { getOwner } from '@ember/application';
 
 export const helper = Helper.extend({
   text: 'abc',
+  init(...args) {
+    this._super(...args);
+    const o = getOwner(this);
+    if (!o) throw new Error('no owner');
+  },
   compute() {
     if (this.text === 'abc') {
       setTimeout(() => {
