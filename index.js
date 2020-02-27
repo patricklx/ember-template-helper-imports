a33fc69ea2344c5adb08dd44bcf44c51380bfa34
patricklx <patricklx@users.noreply.github.com>
2020-02-27 10:33:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const fs = require('fs');
 const BroccoliFilter = require('broccoli-persistent-filter');
 
-const IMPORT_PATTERN = /\{\{\s*import\s+([a-z\-,\s*]+)*\s+from\s+['"]([^'"]+)['"]\s*\}\}/g;
+const IMPORT_PATTERN = /\{\{\s*import\s+([a-z\-,\s*'"]+)*\s+from\s+['"]([^'"]+)['"]\s*\}\}/g;
 
 function isValidVariableName(name) {
   if (!(/^[A-Za-z0-9.\-]+$/.test(name))) {
